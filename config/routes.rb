528343775e9ffc563957a660f54a0a17da97882b
Jace P. Gold <Jace@JaceGold.com>
2017-11-29 14:52:05 -0700

    Add billboards, artists, and songs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/edit'
+
+  get 'songs/new'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/edit'
+
+  get 'artists/new'
+
+  get 'billboards/index'
+
+  get 'billboards/show'
+
+  get 'billboards/edit'
+
+  get 'billboards/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
